c893e02e4bb848e9504c0d5c3c4b0bfdde424c11
JackM400 <jack.millar400@gmail.com>
2019-08-11 20:26:45 +0100

    Clear Functionality added

--- src/sample/FXMLController.java
+++ src/sample/FXMLController.java
@@ -4,22 +4,48 @@ import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+
+import javax.swing.*;
 
 public class FXMLController implements Initializable {
     private boolean numberInput;
     private BigDecimal left;
     private String selectedOpeeration;
+
+    public FXMLController() {
+        this.numberInput = false;
+        this.left = BigDecimal.ZERO;
+        this.selectedOpeeration = "";
+    }
+
+    @FXML
+    public void buttonOperation(ActionEvent evt) {
+        Button buttonn = (Button) evt.getSource();
+        String buttonText = buttonn.getText();
+
+        if (buttonText.equals("C")) {
+            if (buttonText.equals("C")) {
+                left = BigDecimal.ZERO;
+
+            }
+            selectedOpeeration = "";
+            TextField.clear();
+            numberInput = true;
+            return;
+        }
+    }
+
+    @FXML
+    private TextField TextField;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
 
-    public FXMLController(BigDecimal left, String selectedOpeeration, boolean numberInput) {
-        this.numberInput = numberInput;
-        this.left = left;
-        this.selectedOpeeration = selectedOpeeration;
-    }
 }
