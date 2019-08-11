a15934b239bc3735d552ea253d53e9fc58d8af16
JackM400 <jack.millar400@gmail.com>
2019-08-11 20:09:59 +0100

    Buttons added , resized , try catch added

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,12 +9,16 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        //primaryStage.setScene(new Scene(root, 400, 160));
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+            primaryStage.setTitle("Hello World");
+            //primaryStage.setScene(new Scene(root, 400, 160));
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
